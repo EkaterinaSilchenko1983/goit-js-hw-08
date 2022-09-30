56e89501e3a795f0f48f52f2a4eda22cc0e5226e
Ekaterina Silchenko <maksil.17@gmail.com>
2022-09-30 19:10:23 +0300

    add gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,28 @@
 // Add imports above this line
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
-console.log(SimpleLightbox);
+// console.log(SimpleLightbox);
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const markup = galleryItems.reduce(
+  (acc, item) =>
+    acc +
+    `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`,
+  ''
+);
+
+galleryEl.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
