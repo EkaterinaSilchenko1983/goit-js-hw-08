607e0ec30a40b78d7ae5b7881f916cbd560c9856
Ekaterina Silchenko <maksil.17@gmail.com>
2022-10-02 23:22:43 +0300

    add 3 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -13,16 +13,4 @@ const onPlay = function (data) {
 };
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player
-  .setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY))
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY) || 0);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+const form = document.querySelector('.feedback-form');
+const btn = document.querySelector('button');
+const textarea = document.querySelector('textarea');
+
+cheksStorage();
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', onFormInput);
+
+function onFormSubmit(evt) {
+  evt.prevantDefault();
+}
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  console.log(formData);
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+function cheksStorage() {
+  const savedData = localStorage.getItem(LOCALSTORAGE_KEY);
+  const parseSavedData = JSON.parse(savedData);
+  console.log(parseSavedData);
+  console.log(formData[evt.target.value]);
+}
